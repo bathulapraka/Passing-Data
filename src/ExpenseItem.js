de61547df9cde89dec4422f5b73ad2aaddb473f6
bathulapraka <bathulaprakash406@gmail.com>
2024-01-18 18:05:14 +0530

    date and data of the particular user detaills i also added price of the choosen product

---
+++ src/ExpenseItem.js
@@ -0,0 +1,18 @@
+
+
+function App(props){
+
+  return (
+  <div className="alldetails">
+    <div className="expense-item">
+      <div>{props.date.toDateString()}</div>
+    <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+ </div>
+    </div>
+    </div>
+  );
+};
+
+export default App;
